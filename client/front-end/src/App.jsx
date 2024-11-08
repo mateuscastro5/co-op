d7a005070b84af22275053c8e79fc0d0a3670262
Vitorm <vitordevveiga.com>
2024-11-08 14:34:05 -0300

    Initial Front-end commit

--- client/front-end/src/App.jsx
+++ client/front-end/src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
+import { Header } from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+    <Header />
     </>
   )
 }
---
+++ client/front-end/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+export function Header() {
+    return (
+        <header className="header">
+            <div className="header-container">
+                <h1>co-op</h1>
+                <nav>
+                    <button>Descobrir <img src="" alt="" /></button>
+                    <ul>
+                        <li>Login In</li>
+                        <li>Sign In</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+
+}
